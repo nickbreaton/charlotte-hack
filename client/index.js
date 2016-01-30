a536b47f30f317296722f9edae64a2d21589724a
Nick Breaton <nick@breaton.com>
2016-01-30 02:05:18 -0500

    Start something nice because we are almost asleep.

--- client/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-    var steps = {
-      front: $("#front"),
-      location: $("#location")
-    }
-    
-    steps.location.addClass('visible');
-});
