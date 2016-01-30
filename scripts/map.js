d1c9eb3c8ee5b417f7dfc39277047f7162331abc
Nick Breaton <nick@breaton.com>
2016-01-30 10:34:00 -0500

    Add good way to enter multiple hospitals.

--- scripts/map.js
+++ scripts/map.js
@@ -9,24 +9,32 @@ function data (silo) {
 
     // GET /api/name/lat/lng
     $.get('/api/' + silo.attr('id') + '/' + lat + '/' + lng, function (data) {
-      var latlng = JSON.parse(data);
-      console.log(latlng);
-      map(latlng, 10, silo.css('background-color'));
+      var locations = JSON.parse(data);
+      map({ lat: lat, lng: lng }, locations, 7, silo.css('background-color'));
     });
   });
 
 }
 
-function map (latlong, zoom, color) {
+function map (home, locations, zoom, color) {
   var map = new google.maps.Map(document.getElementById('map-inner'), {
-    center: latlong,
+    center: home,
     zoom: zoom,
     styles: [{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}]
   });
 
-  var marker = new google.maps.Marker({
-    position: latlong,
+  // create home maker
+  new google.maps.Marker({
+    position: home,
     map: map,
-    icon: '/pin.svg/' + color
+    icon: '/pin.svg/' + color + '/' + true
+  });
+
+  locations.forEach(function (loc) {
+    new google.maps.Marker({
+      position: loc,
+      map: map,
+      icon: '/pin.svg/' + color + '/' + false
+    });
   });
 }
--- server/api.js
+++ server/api.js
@@ -2,9 +2,22 @@ module.exports = function (app) {
   app.get('/api/:id/:lat/:lng', function (req, res) {
 
     // temporarily reuse their location
-    res.end(JSON.stringify({
-      lat: Number(req.params.lat),
-      lng: Number(req.params.lng)
-    }));
+    res.end(JSON.stringify([
+      {
+        lat: Number(req.params.lat) - 1,
+        lng: Number(req.params.lng) + 1,
+        name: 'Some Hospital'
+      },
+      {
+        lat: Number(req.params.lat) + 1.2,
+        lng: Number(req.params.lng) + 0.75,
+        name: 'Another Hospital'
+      },
+      {
+        lat: Number(req.params.lat) - 2.2,
+        lng: Number(req.params.lng) + 0.8,
+        name: 'A Third Hospital'
+      }
+    ]));
   });
 }
