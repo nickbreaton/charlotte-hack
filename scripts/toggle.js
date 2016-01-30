a9b929aee2b08ebea69675d065eb42b7f057a75a
Nick Breaton <nick@breaton.com>
2016-01-30 04:57:19 -0500

    Add proper slider

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,8 +1,32 @@
 window.addEventListener('load', function () {
   var silos = $('.silo');
+  var overlay = $('#overlay');
 
-  silos.click(function (e) {
-    var silo =  $(e.toElement);
+  silos.on('click',function (e) {
+    // query clicked element
+    var silo = $(e.toElement);
+
+    // find parent up chain
+    while (!silo.hasClass('silo')) {
+      silo = $(silo.parent());
+    }
+
+    // toggle class on and off
     silo.toggleClass('active');
+
+    // remove overlay if not clicked
+    overlay.removeClass('active');
+
+    // enable overlay on done
+    var observer = setInterval(function () {
+      if (silo.width() == window.innerWidth && silo.hasClass('active')) {
+          // clear out observer
+          clearInterval(observer);
+
+          // display menu
+          overlay.addClass('active');
+      }
+    }, 100);
   });
+
 });
